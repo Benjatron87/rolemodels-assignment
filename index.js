481a5bc5698f2312998ffe19210d5ddf8119516a
Benjatron <benjatron@MacBook-Air-4.local>
2023-07-02 13:50:05 -0400

    First commit to add project files

---
+++ index.js
@@ -0,0 +1,8 @@
+import * as THREE from 'three'
+import ExampleModel from './models/ExampleModel.js'
+
+// 3D Example:
+const shape = new THREE.BoxGeometry(5, 5, 5)
+application.add3DShape(shape, '#FF7800')
+
+
